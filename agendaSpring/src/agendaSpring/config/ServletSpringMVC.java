e3e84491b8ddfaf46fdb2162ea04ccd693a54c40
20151074010212 <20151074010212@MC266457.ifrn.local>
2018-10-01 08:27:53 -0300

    criando o "servelet" dispatcher

---
+++ agendaSpring/src/agendaSpring/config/ServletSpringMVC.java
@@ -0,0 +1,26 @@
+package agendaSpring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
