c768fa3dca8819f5ae0848b94db1966eb1af1a70
20151074010212 <20151074010212@MC266457.ifrn.local>
2018-10-01 09:31:14 -0300

    Criamos pasta views dentro da web-inf, criou um jsp, home. jsp e configurou o spring para entender onde as paginas da aplicação estão. InternalResolcerViewsResolver

--- agendaSpring/src/agendaSpring/config/AppWebConfiguration.java
+++ agendaSpring/src/agendaSpring/config/AppWebConfiguration.java
@@ -1,12 +1,21 @@
 package agendaSpring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import agendaSpring.controllers.HomeController;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses= {HomeController.class})
 public class AppWebConfiguration {
+	
+	@Beanean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver Resolver = new InternalResourceViewResolver();
+		Resolver.setPrefix("/WEB_INF/views/");
+		return Resolver;
+	}
 
 }
